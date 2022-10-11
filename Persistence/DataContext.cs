2f0ab3f69033fc559a3c20210de942e6b2e71001
Prakash Kumar Ojha <prakashko@examroom.ai>
2022-10-11 13:50:35 +0530

    First commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option) : base(option)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
