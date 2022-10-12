288fc152dda2bac9c319af4b9407f08a9cf005b3
Prakash Kumar Ojha <prakashko@examroom.ai>
2022-10-12 14:58:04 +0530

    Edit, and delete Handler with controller added

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -3,6 +3,7 @@ using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Persistence;
+using System.ComponentModel;
 
 namespace API.Controllers
 {
@@ -26,7 +27,19 @@ namespace API.Controllers
         public async Task<IActionResult> CreateActivity(Activity activity) // returning nothing thats why we used <IActionResult>
         {
             return Ok(await Mediator.Send(new Create.Command { Activity = activity })); //Activity - variable for Activity class which is created in Create class inside Activites folder, 
-                                                                                        //activity - is parameter for Activity domain class
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command { Id = id }));
         }
     }
 }
---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Domain;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Activity = Domain.Activity;
+using System.Diagnostics;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+
+                if (activity != null)
+                {
+                    activity.Title = request.Activity.Title;
+                    activity.Date = request.Activity.Date;
+                    activity.Description = request.Activity.Description;
+                    activity.Category = request.Activity.Category;
+                    activity.City = request.Activity.City;
+                    activity.Venue = request.Activity.Venue;
+                }
+
+                _context.Activities.Update(activity);
+                await _context.SaveChangesAsync();
+                return Unit.Value;
+            }
+        }
+    }
+}
